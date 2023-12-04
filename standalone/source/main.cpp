0f837477472f9c1022917ca889231b74e3ce63ed
Joseph Shunk <joseph.shunk@gmail.com>
2023-12-03 19:13:20 -0800

    Grab a sprite, to eventually be used to generate collision from.

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,9 +1,13 @@
 import stk.collide;
+import stk.sprite;
+import <SFML/Graphics.hpp>;
 
 using namespace stk;
 
 int main()
 {
-	c_collision_mask collision;
+	c_collision_mask collision(128, 128);
+	c_sprite_bank sprites;
+	sprites.make_sprite("rocket"_h, "rocket_small.png", 300, 300);
 	return 0;
 }
