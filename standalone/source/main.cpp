e2be6b48f4023841c3788a8ab149f32e95b719af
Joseph Shunk <joseph.shunk@gmail.com>
2023-12-04 23:20:36 -0800

    Load sprite. Generate collision from sprite image.

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,4 +1,5 @@
 import stk.collide;
+import stk.log;
 import stk.sprite;
 import <SFML/Graphics.hpp>;
 
@@ -8,6 +9,12 @@ int main()
 {
 	c_collision_mask collision(128, 128);
 	c_sprite_bank sprites;
-	sprites.make_sprite("rocket"_h, "rocket_small.png", 300, 300);
+	sf::Sprite* rocket_sprite = sprites.make_sprite("rocket"_h, "data/rocket_small.png", 300, 300);
+	if (rocket_sprite == nullptr)
+	{
+		errorln("Failed to load rocket sprite");
+		return 1;
+	}
+	collision.from_image(rocket_sprite->getTexture()->copyToImage());
 	return 0;
 }
